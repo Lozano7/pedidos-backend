27558553a7be3bad4636e0272a8faf546f65366a
alozano-links <56984985+Lozano7@users.noreply.github.com>
2024-01-23 19:27:33 -0500

    fixs cors

--- src/constants/cors.ts
+++ src/constants/cors.ts
@@ -1,31 +1,7 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const CORS: CorsOptions = {
-  origin: ['https://pedidos-backend-bbln.vercel.app'],
+  origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: [
-    'Content-Type',
-    'Authorization',
-    'credentials',
-    'X-Requested-With',
-    'Accept',
-    'Origin',
-    'cors',
-    'cookie',
-    'Access-Control-Allow-Headers',
-    'Access-Control-Allow-Origin',
-    'Access-Control-Allow-Credentials',
-  ],
-  exposedHeaders: [
-    'Authorization',
-    'credentials',
-    'Access-Control-Allow-Headers',
-    'Access-Control-Allow-Origin',
-    'Access-Control-Allow-Credentials',
-    'Content-Type',
-    'X-Requested-With',
-    'Accept',
-    'Origin',
-    'cors',
-  ],
+  credentials: true,
 };
